433ada2f6efc7600dafb25b3d4c9aef230fa8fbf
De Shin Li <s4492581@labs.eait.uq.edu.au>
2019-10-12 14:57:15 +1000

    soluiton

--- a2-handout-2019/src/assignment2/Dynamic.java
+++ a2-handout-2019/src/assignment2/Dynamic.java
@@ -1,5 +1,8 @@
 package assignment2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dynamic {
 
     private static int[][][] storage;
@@ -52,7 +55,6 @@ public class Dynamic {
         for (int i = jobs.length - 1; i > 0; i--) {
             // work backwards to find the possible start of the shift
             start = getStartJob(i, maxShiftLength, jobs);
-            System.out.println(start);
             for (int j = start; j < i; j++) {
                 for (int k = j; k < i; k++) {
                     // take or not take first
@@ -166,6 +168,14 @@ public class Dynamic {
         return jobs.length;
     }
 
+    private static int getCompromise(int i, int k, Job[] jobs) {
+        int compromise = 0;
+        for (int jobIndex = k + 1; jobIndex < i ; jobIndex++) {
+            compromise += jobs[jobIndex].payment();
+        }
+        return compromise;
+    }
+
     /**
      * @require The array cost is not null, and all the integers in the array
      *          (the daily costs of the worker) are greater than or equal to
@@ -192,6 +202,175 @@ public class Dynamic {
      */
     public static Solution optimalSolutionDynamic(int[] cost, int minShiftBreak,
             int maxShiftLength, Job[] jobs) {
-        return null; // REMOVE THIS LINE AND IMPLEMENT THIS METHOD
+        int[][][] storage =
+                new int[jobs.length + 1][jobs.length + 1][jobs.length + 1];
+
+        // lowest level
+        storage[jobs.length][jobs.length][jobs.length] = 0;
+        int start = getStartJob(jobs.length, maxShiftLength, jobs);
+        for (int i = start; i < jobs.length; i++) {
+            for (int j = i; j < jobs.length; j++) {
+                int profit = 0;
+                // lowest level cost, last job must be compatible to first
+                // and work you way to the start find compatible ones
+                int compromise = 0;
+                for (int jobIndex = j + 1; jobIndex < jobs.length ; jobIndex++) {
+                    compromise += jobs[jobIndex].payment();
+                }
+                storage[jobs.length][i][j] = Math.max(getProfit(cost, jobs, i
+                        , j, jobs.length) + compromise, 0) ;
+            }
+        }
+        for (int i = jobs.length - 1; i > 0; i--) {
+            // work backwards to find the possible start of the shift
+            start = getStartJob(i, maxShiftLength, jobs);
+            for (int j = start; j < i; j++) {
+                for (int k = j; k < i; k++) {
+                    // take or not take first
+                    // new shift out of length, dont take it
+                    // in range max(take, not)
+                    storage[i][jobs.length][jobs.length] =
+                            jobs[i].end() - jobs[i].start() + 1 > maxShiftLength ?
+                                    storage[i+1][jobs.length][jobs.length]:
+                                    Math.max(storage[i+1][i][i],
+                                            storage[i+1][jobs.length][jobs.length]);
+
+                    int nextJob = getNextJobAfterBreak(jobs, minShiftBreak, k);
+                    if (jobs[k].end() < jobs[i].start()) {
+                        // no overlap
+                        if (jobs[i].end() - jobs[j].start() + 1 > maxShiftLength) {
+                            // newShift
+                            int compromise = 0;
+                            for (int jobIndex = k + 1; jobIndex < i ; jobIndex++) {
+                                compromise += jobs[jobIndex].payment();
+                            }
+                            storage[i][j][k] = getProfit(cost, jobs, j, k, i) +
+                                    storage[nextJob][jobs.length][jobs.length]
+                                    + compromise;
+                        } else {
+                            // max(take, newShift, skip)
+                            int take = storage[i+1][j][i];
+                            int compromise = 0;
+                            for (int jobIndex = k + 1; jobIndex < i ; jobIndex++) {
+                                compromise += jobs[jobIndex].payment();
+                            }
+                            int newShift = getProfit(cost, jobs, j, k, i) +
+                                    storage[nextJob][jobs.length][jobs.length]
+                                    + compromise;
+                            int skip = storage[i+1][j][k] - jobs[i].payment();
+                            storage[i][j][k] = Math.max(skip, Math.max(take,
+                                    newShift));
+                        }
+                    } else {
+                        // overlap
+                        if (jobs[i].end() - jobs[j].start() + 1 > maxShiftLength) {
+                            // newShift
+                            int compromise = 0;
+                            for (int jobIndex = k + 1; jobIndex < i; jobIndex++) {
+                                compromise += jobs[jobIndex].payment();
+                            }
+                            storage[i][j][k] = getProfit(cost, jobs, j, k, i) +
+                                    storage[nextJob][jobs.length][jobs.length]
+                                    + compromise;
+                        } else {
+                            // max(skip, newShift)
+                            int skip = storage[i + 1][j][k] - jobs[i].payment();
+                            int compromise = 0;
+                            for (int jobIndex = k + 1; jobIndex < i; jobIndex++) {
+                                compromise += jobs[jobIndex].payment();
+                            }
+                            int newShift = getProfit(cost, jobs, j, k, i) +
+                                    storage[nextJob][jobs.length][jobs.length]
+                                    + compromise;
+                            storage[i][j][k] = Math.max(skip, newShift);
+                        }
+                    }
+                }
+            }
+        }
+
+        int i = 0; int j = jobs.length; int k = jobs.length;
+        List<Shift> shifts = new ArrayList<>();
+        List<Job> chosenJobs = new ArrayList<>();
+        List<Job> jobsInShift = new ArrayList<>();
+        while(i != jobs.length) {
+            if (j == jobs.length) {
+                // start of a new shift
+                if (jobs[i].length() > maxShiftLength) {
+                   i++;
+                } else {
+                    int take = storage[i+1][i][i];
+                    int newShift = storage[i+1][jobs.length][jobs.length];
+                    if (take > newShift) {
+                        jobsInShift.add(jobs[i]);
+                        chosenJobs.add(jobs[i]);
+                        j = i;
+                        k = i;
+                        i++;
+                    } else {
+                        i++;
+                    }
+                }
+            } else {
+                int nextJob = getNextJobAfterBreak(jobs, minShiftBreak, k);
+                if (jobs[i].compatible(jobs[k])) {
+                    // compatible
+                    if (jobs[i].end() - jobs[j].start() + 1 > maxShiftLength) {
+                        j = jobs.length;
+                        k = jobs.length;
+                        shifts.add(new Shift(jobsInShift.get(0).start(), jobsInShift.get(jobsInShift.size()-1).end()));
+                        jobsInShift.clear();
+                        i++;
+                    } else {
+                        int take = storage[i+1][j][i];
+                        int skip = storage[i+1][j][k] - jobs[i].payment();
+                        int newShift = getProfit(cost, jobs, j, k ,i) + storage[nextJob][jobs.length][jobs.length] +
+                                getCompromise(i, k, jobs);
+                        int max = Math.max(take, Math.max(newShift, skip));
+                        if (max == take) {
+                            jobsInShift.add(jobs[i]);
+                            chosenJobs.add(jobs[i]);
+                            k = i;
+                            i++;
+                        } else if (max == skip) {
+                            i++;
+                        } else {
+                            j = jobs.length;
+                            k = jobs.length;
+                            shifts.add(new Shift(jobsInShift.get(0).start(), jobsInShift.get(jobsInShift.size()-1).end()));
+                            jobsInShift.clear();
+                            i++;
+                        }
+                    }
+                } else {
+                    // not compatible
+                    if (jobs[i].end() - jobs[j].start() + 1 > maxShiftLength) {
+                        j = jobs.length;
+                        k = jobs.length;
+                        shifts.add(new Shift(jobsInShift.get(0).start(), jobsInShift.get(jobsInShift.size()-1).end()));
+                        jobsInShift.clear();
+                        i++;
+                    } else {
+                        int skip = storage[i+1][j][k] - jobs[i].payment();
+                        int newShift = getProfit(cost, jobs, j, k ,i) + storage[nextJob][jobs.length][jobs.length] +
+                                getCompromise(i, k, jobs);
+                        if (skip > newShift) {
+                            i++;
+                        } else {
+                            j = jobs.length;
+                            k = jobs.length;
+                            shifts.add(new Shift(jobsInShift.get(0).start(), jobsInShift.get(jobsInShift.size()-1).end()));
+                            jobsInShift.clear();
+                            i++;
+                        }
+                    }
+                }
+            }
+        }
+        if (!jobsInShift.isEmpty()) {
+            shifts.add(new Shift(jobsInShift.get(0).start(), jobsInShift.get(jobsInShift.size()-1).end()));
+        }
+        System.out.println(new Solution(shifts, chosenJobs));
+        return new Solution(shifts, chosenJobs);
     }
 }
